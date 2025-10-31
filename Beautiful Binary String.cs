b3b706ce2f14d3651b66b7db094a2918cb435288
Jallyn <jallynribaya979@gmail.com>
2025-10-31 13:08:48 +0800

    Create Beautiful Binary String.cs

---
+++ Beautiful Binary String.cs
@@ -0,0 +1,66 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'beautifulBinaryString' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts STRING b as parameter.
+     */
+
+    public static int beautifulBinaryString(string b)
+    {
+        int count = 0;
+        int i = 0;
+
+        while (i < b.Length - 2)
+        {
+           
+            if (b.Substring(i, 3) == "010")
+            {
+                count++;
+                i += 3; 
+            }
+            else
+            {
+                i++;
+            }
+        }
+
+        return count;
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+        string b = Console.ReadLine();
+
+        int result = Result.beautifulBinaryString(b);
+
+        textWriter.WriteLine(result);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
